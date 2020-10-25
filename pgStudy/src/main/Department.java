4a40e98fc58c517dec790e5e3741e93c6d102803
user <user@LAPTOP-7BULUF2G>
2020-10-26 00:35:42 +0900

    study commit

---
+++ pgStudy/src/main/Department.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Department {
+	
+	private String deptNo;
+	private String deptName;
+	
+	public Department() {}
+	
+	public Department(String deptNo, String deptName) {
+		super();
+		this.deptNo = deptNo;
+		this.deptName = deptName;
+	}
+	
+	public String getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(String deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+
+}
